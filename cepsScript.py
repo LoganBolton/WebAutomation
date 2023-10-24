bd2606203d74ee1a73ba6435c4379019c49aff64
Logan Bolton <loganbolton101@gmail.com>
2023-10-24 08:02:04 -0500

    clean up code

--- cepsScript.py
+++ cepsScript.py
@@ -3,23 +3,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
+
 password = os.environ.get('S_PASSWORD')
-if password:
-    print("Password retrieved!")
-else:
-    print("man")
-    
 driver = webdriver.Firefox()
 # driver.get("https://bstackdemo.com/")
 # driver.get("https://cws.auburn.edu/ceps/")
 driver.get("http://debug.auburn.edu:8080/ceps")
-print(password)
 wait = WebDriverWait(driver, 5)  # Wait for up to 5 seconds
-importPassword = password
-print(importPassword)
+
+
 # Wait until the element with the class "btn btn-default" is present
 element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "btn")))
-
 element.click()
 
 # Fill in login info
@@ -27,7 +21,6 @@ username_element = wait.until(EC.presence_of_element_located((By.ID, "username")
 password_element = wait.until(EC.presence_of_element_located((By.ID, "password")))
 username_element.send_keys("ldb0046")  # Replace 'YourDesiredTextHere' with the text you want to enter
 password_element.send_keys(password)  # Replace 'YourDesiredTextHere' with the text you want to enter
-
 
 element = wait.until(EC.presence_of_element_located((By.NAME, "submit")))
 element.click()
@@ -50,8 +43,6 @@ second_radio_button = labels_with_no[1].find_element(By.XPATH, './/input')
 second_radio_button.click()
 
 saveButton = driver.find_element(By.XPATH, '//button[text()="Save and Continue"]')
-
-
 # Find the button with the text "Save and Continue" and click on it
 saveButton.click()
 
